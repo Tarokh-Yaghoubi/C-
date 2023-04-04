45e13e191f7304d531de822ad6fbf6b5c3de6926
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2023-04-04 12:38:01 +0430

    sub, sum, multiplication and division in C++ Programming

---
+++ basic_math.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, sum, sub, multiplication;
+    float division;
+
+    cout << "Enter the values for a and b : " << endl;
+    cin >> a >> b;
+
+    sum = a + b;
+    sub = a - b;
+    multiplication = a * b;
+    division = (float) a / b;
+
+    cout << "Sum = " << sum << endl;
+    cout << "Sub = " << sub << endl;
+    cout << "Div = " << division << endl;
+    cout << "Multiplication = " << multiplication << endl;
+
+    system("pause");
+    return 0;
+
+}
