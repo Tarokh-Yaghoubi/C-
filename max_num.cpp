78b94acce60c38f14f3dc487b327fb8d51951dc7
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2023-04-04 12:54:46 +0430

    detect the biggest number among three numbers

---
+++ max_num.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int max(int a, int b, int c);
+
+int main()
+{
+    int a, b, c;
+    int ans = 0;
+    cout << "Enter the values of a, b, c :" << endl;
+    cin >> a >> b >> c;
+    ans = max(a, b, c);
+
+    cout << "The answer is : " << ans << endl;
+    system("pause");
+    return 0;
+}
+
+int max(int a, int b, int c)
+{
+    int max = 0;
+
+    max = a;
+    if (b > max)
+        max = b;
+    else if (c > max)
+        max = c;
+    
+    return max;
+}
