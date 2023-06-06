b4ea005019a3c2881b4c3b7ea7878075a03276ff
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-06-06 19:48:07 +0330

    Check to see if the number is Odd or Even , Using Shorthand

---
+++ university-programs/OddOrEven.cpp
@@ -0,0 +1,21 @@
+/*
+
+	Author : Tarokh Yaghoubi
+	Date :
+
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string result;
+	int number;
+	cout << "Enter the number to see if it is odd or even : ";
+	cin >> number;
+	result = (number % 2 == 0) ? "Even" : "Odd";
+	cout << result;
+}
