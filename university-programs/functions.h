5b91d36d97a37090d84cd8b59435fc0148dfd486
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-06-09 18:16:34 +0330

    my header file

---
+++ university-programs/functions.h
@@ -0,0 +1,15 @@
+/*
+
+	Bubble Sort Header File
+	Author : Tarokh Yaghoubi
+	Date : 6/9/2023
+
+*/
+
+#ifndef MY_FUNCTIONS_H
+#define MY_FUNCTIONS_H
+
+void bubbleSort(int numbers[], int len);
+int binarySearch(int arr[], int left, int right, int x);
+
+#endif // !MY_FUNCTIONS_H
