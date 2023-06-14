91fe92d1db1772557b4237b26f9bf244b9118c43
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2023-06-14 10:50:15 +0330

    Rectangle Class Declaration , Using a Constructor to do the operation , C++ programming

---
+++ university-programs/Classes/RectangleArea_Constructor/rectangle.h
@@ -0,0 +1,25 @@
+
+#ifndef REC_CONST_H
+
+#define REC_CONST_H
+
+#include <string>
+#include <iostream>
+
+class Rectangle {
+
+private:
+
+	int len;
+	int width;
+
+public:
+
+	Rectangle();
+	Rectangle(int len, int width);
+	~Rectangle();
+
+};
+
+#endif
+
