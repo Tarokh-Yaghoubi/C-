ac298da5d344e10b0869c0601481d99bd83229eb
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2023-06-14 09:17:24 +0330

    Main Function of the program , C++ programming

---
+++ university-programs/Classes/Person_Constructor/main.cpp
@@ -0,0 +1,14 @@
+
+#include "person.h"
+
+using namespace std;
+
+int main()
+{
+	string theName = "Tarokh";
+	int theAge = 19;
+	Person person1(theName, theAge);
+
+	system("pause");
+	return 0;
+}
