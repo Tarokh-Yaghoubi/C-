7b6d2b5f2ff941854810facc06c94072f47f2b89
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-06-16 15:07:11 +0330

    Take two numbers and print out the sum of them , C++ programming

---
+++ Learning Primer/chapter_one/IO_lib1.cpp
@@ -0,0 +1,21 @@
+// A program that uses the IO Library 
+
+/*
+
+	Author : Tarokh Yaghoubi
+	Date :
+
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Enter two numbers : " << std::endl;
+	int v1 = 0, v2 = 0;
+	std::cin >> v1 >> v2;
+	std::cout << "The sum of v1 and v2 is " << v1 + v2 << std::endl;
+
+	return 0;
+}
+
