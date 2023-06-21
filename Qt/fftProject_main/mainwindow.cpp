dc78349c251691b9cbd86ef025ac95c71cda07e4
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-06-21 11:40:26 +0330

    Defined generate , read and write buttons

--- Qt/fftProject_main/mainwindow.cpp
+++ Qt/fftProject_main/mainwindow.cpp
@@ -13,3 +13,78 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+float randomFloat()
+{
+    return (float)(rand()) / (float)(rand());
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    this->close();
+}
+
+
+void MainWindow::on_pushButton_generate_clicked()
+{
+    QFile File("myfilewrite.txt");
+
+    std::vector<float>numbers;
+    std::srand(std::time(nullptr));
+
+    float randNum;
+
+
+    if (File.open(QIODevice::Truncate | QIODevice::ReadWrite))
+    {
+        QTextStream stream(&File);
+        for (int i = 0; i < 10240; i++)
+        {
+            randNum = randomFloat();
+            numbers.push_back(randNum);
+        }
+        // append to the textBox
+        for (int i = 0; i < 10240; i++)
+        {
+            QString currentRandNum = QString::number(numbers[i], 'f', 3);
+            ui->textEdit_writeFile->append(currentRandNum);
+        }
+    }
+
+    File.close();
+}
+
+
+void MainWindow::on_pushButton_write_clicked()
+{
+
+    QFile File("myfilewrite.txt");
+
+    if (File.open(QIODevice::Truncate | QIODevice::ReadWrite))
+    {
+        QTextStream stream(&File);
+        stream << ui->textEdit_writeFile->toPlainText();
+    }
+    File.close();
+}
+
+
+void MainWindow::on_pushButton_read_clicked()
+{
+
+    QFile File("myfilewrite.txt");
+
+    if (File.open(QIODevice::ReadOnly))
+    {
+        QTextStream stream(&File);
+        QString FileData;
+        while (stream.atEnd() == false)
+        {
+            FileData += stream.readLine() + "\n";
+        }
+        ui->textEdit->setText(FileData);
+
+    }
+}
+
