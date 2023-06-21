9a20646920a06806edf4a0d4b89b20eeced90d45
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-06-21 11:42:56 +0330

    Build Files

---
+++ Qt/build-fftProject_main-Desktop_Qt_6_5_1_MinGW_64_bit-Debug/fftProject_main_autogen/include/ui_mainwindow.h
@@ -0,0 +1,92 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.5.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QPushButton *pushButton;
+    QPushButton *pushButton_read;
+    QPushButton *pushButton_generate;
+    QPushButton *pushButton_write;
+    QTextEdit *textEdit;
+    QTextEdit *textEdit_writeFile;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(800, 731);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName("pushButton");
+        pushButton->setGeometry(QRect(350, 620, 80, 61));
+        pushButton_read = new QPushButton(centralwidget);
+        pushButton_read->setObjectName("pushButton_read");
+        pushButton_read->setGeometry(QRect(70, 70, 80, 71));
+        pushButton_generate = new QPushButton(centralwidget);
+        pushButton_generate->setObjectName("pushButton_generate");
+        pushButton_generate->setGeometry(QRect(350, 70, 80, 71));
+        pushButton_write = new QPushButton(centralwidget);
+        pushButton_write->setObjectName("pushButton_write");
+        pushButton_write->setGeometry(QRect(600, 70, 80, 71));
+        textEdit = new QTextEdit(centralwidget);
+        textEdit->setObjectName("textEdit");
+        textEdit->setGeometry(QRect(60, 170, 311, 241));
+        textEdit_writeFile = new QTextEdit(centralwidget);
+        textEdit_writeFile->setObjectName("textEdit_writeFile");
+        textEdit_writeFile->setGeometry(QRect(430, 170, 301, 241));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 800, 21));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Close", nullptr));
+        pushButton_read->setText(QCoreApplication::translate("MainWindow", "Read", nullptr));
+        pushButton_generate->setText(QCoreApplication::translate("MainWindow", "Generate", nullptr));
+        pushButton_write->setText(QCoreApplication::translate("MainWindow", "Write", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
