50a024b5e48dc2fcd0bf0def0e8460a3f318e1f0
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-06-21 13:51:07 +0330

    defined chart_button

--- Qt/fftProject_main/mainwindow.cpp
+++ Qt/fftProject_main/mainwindow.cpp
@@ -88,3 +88,48 @@ void MainWindow::on_pushButton_read_clicked()
     }
 }
 
+
+void MainWindow::on_pushButton_chart_clicked()
+{
+
+    //![1]
+    //!     //![1]
+    QSplineSeries *series = new QSplineSeries();
+    series->setName("spline");
+    //![1]
+
+    //![2]
+    series->append(1, 2);
+    series->append(2, 4);
+    series->append(3, 4);
+    series->append(3, 4);
+    series->append(1, 9);
+    *series << QPointF(11, 1) << QPointF(13, 3) << QPointF(17, 6) << QPointF(18, 3) << QPointF(20, 2);
+    //![2]
+
+    //![3]
+    QChart *chart = new QChart();
+    chart->legend()->hide();
+    chart->addSeries(series);
+    chart->setTitle("Simple spline chart example");
+    chart->createDefaultAxes();
+    chart->axes(Qt::Vertical).first()->setRange(0, 10);
+    //![3]
+
+    //![4]
+    QChartView *chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+    chartView->setAttribute(Qt::WA_DeleteOnClose, false);
+    //![4]
+
+    //![5]
+
+    // It didn't work because it was defined on the stack ... so i used new to bring it on heap memory .
+
+    QMainWindow *window = new QMainWindow();
+    window->setCentralWidget(chartView);
+    window->resize(400, 300);
+    window->show();
+
+}
+
