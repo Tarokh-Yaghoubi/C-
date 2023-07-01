8bb2e9a5b6e49b826d7355bdc83520802afb6378
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-07-01 22:53:34 +0330

    pushed all

--- Learning Primer/chapter_two/first_program.cpp
+++ Learning Primer/chapter_two/first_program.cpp
@@ -4,13 +4,14 @@ int outer_var = 10;
 
 int main()
 {
+
     std::cout << outer_var << std::endl;
     int outer_var = 5;
 
     std::cout << outer_var << std::endl;
 
     std::cout << ::outer_var << std::endl;
-
+    
     return 0;
 
 }
