313e216b4cea786b3877056f7ea28029db16cbb0
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.COM>
2023-09-19 10:05:03 +0330

    added a line for testing cerr

--- Learning Primer/chapter_one/IO_lib1.cpp
+++ Learning Primer/chapter_one/IO_lib1.cpp
@@ -13,6 +13,9 @@ int main()
 {
 	std::cout << "Enter two numbers : " << std::endl;
 	int v1 = 0, v2 = 0;
+	if (v1 == 0)
+		std::cerr << "This is an error message \n";		/* CERR Test */
+ 
 	std::cin >> v1 >> v2;
 	std::cout << "The sum of v1 and v2 is " << v1 + v2 << std::endl;
 
