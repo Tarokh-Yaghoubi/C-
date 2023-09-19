7a05a6b4fda9b6f913323ed17cab5f234ae7c120
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.COM>
2023-09-19 15:27:56 +0330

    Summing practice using for loops

---
+++ Learning Primer/chapter_one/sum_for.cxx
@@ -0,0 +1,19 @@
+
+/* Sum 1 through 10 inclusively using a for loop */
+
+#include <iostream>
+
+int main()
+{
+
+	int sum = 0;
+
+	for (int val = 1; val <= 10; ++val)
+		sum += val;
+
+	std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
+
+	return 0;
+
+}
+
