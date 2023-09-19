d2a8a49cf1b9e877d64db24f7a0f82d86146598b
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.COM>
2023-09-19 15:30:12 +0330

    Input/Output Practice in C++

---
+++ Learning Primer/chapter_one/IO_cin.c
@@ -0,0 +1,13 @@
+
+// a program that uses the IO library 
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Enter two nunbers : " << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+    return 0;
+}
